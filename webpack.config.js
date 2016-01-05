7c3eda2c509b5f72b8f12303afe862979136b081
hisplan <jaeyoung.chun@gmail.com>
2016-01-05 14:57:39 -0500

    Add react

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 module.exports = {
     entry: {
         main: [
-            './script1.js',
-            './script2.js'
+            './src/main.js'
         ]
     },
     output: {
         filename: './public/[name].js'
+    },
+    module : {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
     }
 }
